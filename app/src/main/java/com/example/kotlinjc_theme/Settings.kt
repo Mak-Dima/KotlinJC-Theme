4aae5287aa8435cfda2c14ed4ad958f3fd1ed3e1
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-07 17:16:47 -0600

    Settings composable fuondation added.

---
+++ app/src/main/java/com/example/kotlinjc_theme/Settings.kt
@@ -0,0 +1,26 @@
+package com.example.kotlinjc_theme
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+
+@Composable
+fun Settings() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Settings")
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun SettingsPreview() {
+    Settings()
+}
