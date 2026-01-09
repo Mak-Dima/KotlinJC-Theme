110ad22b50762f652d00cbcd80fe7cc48b7b9ca2
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-08 19:18:10 -0600

    Settings view updated. Added required elements.

--- app/src/main/java/com/example/kotlinjc_theme/views/Data.kt
+++ app/src/main/java/com/example/kotlinjc_theme/views/Data.kt
@@ -2,14 +2,12 @@ package com.example.kotlinjc_theme.views
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
--- app/src/main/java/com/example/kotlinjc_theme/views/Settings.kt
+++ app/src/main/java/com/example/kotlinjc_theme/views/Settings.kt
@@ -1,21 +1,54 @@
 package com.example.kotlinjc_theme.views
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Settings() {
-    Box(
+    val themes = listOf("Light", "Dark")
+
+    Column(
         modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .fillMaxSize()
+            .padding(50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top)
     ) {
-        Text(text = "Settings")
+        Text(
+            text = "Settings",
+            fontSize = 26.sp
+        )
+
+        themes.forEachIndexed { index, theme ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                RadioButton(
+                    selected = index == 0,
+                    onClick = { /*TODO*/ }
+                )
+                Text(
+                    text = theme,
+                    fontSize = 20.sp
+                )
+            }
+        }
     }
 }
 
