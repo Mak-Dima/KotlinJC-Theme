3916f3e0a76c322e9af12215aad081cfe18bb3fe
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-10 14:03:54 -0600

    Imports removed.

--- app/src/main/java/com/example/kotlinjc_theme/views/Data.kt
+++ app/src/main/java/com/example/kotlinjc_theme/views/Data.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
