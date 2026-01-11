bdb5871c9869148ac34ac1c5f5efed344da5a4f4
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-11 12:33:45 -0600

    MainActivity content moved to separate composable.

--- app/src/main/java/com/example/kotlinjc_theme/MainActivity.kt
+++ app/src/main/java/com/example/kotlinjc_theme/MainActivity.kt
@@ -7,15 +7,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.kotlinjc_theme.dataStore
 import com.example.kotlinjc_theme.ui.theme.KotlinJCThemeTheme
 import com.example.kotlinjc_theme.views.Data
 import com.example.kotlinjc_theme.views.MenuBar
@@ -40,37 +45,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            MainContent( this)
+        }
+    }
+}
 
-            val navController = rememberNavController()
-            val isDarkThemeStored = dataStore.data.map { preferences ->
-                    preferences[IS_DARK_THEME] ?: false
-            }
-            var isDarkTheme = isDarkThemeStored.collectAsState(initial = false).value
 
-            KotlinJCThemeTheme(darkTheme = isDarkTheme) {
-                Scaffold(
-                    bottomBar = {
-                        MenuBar(navController)
-                    },
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = Data
+@Composable
+fun MainContent(context: Context) {
+    val navController = rememberNavController()
+    val isDarkThemeStored = context.dataStore.data.map { preferences ->
+        preferences[IS_DARK_THEME] ?: false
+    }
+    var isDarkTheme = isDarkThemeStored.collectAsState(initial = false).value
+
+    KotlinJCThemeTheme(darkTheme = isDarkTheme) {
+        Scaffold(
+            bottomBar = {
+                MenuBar(navController)
+            },
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = Data
+            ) {
+                composable<Data> { Data() }
+                composable<Settings> {
+                    Settings(
+                        isDarkTheme
                     ) {
-                        composable<Data> { Data() }
-                        composable<Settings> {
-                            Settings(
-                                isDarkTheme,
-                                {
-                                    isDarkTheme = !isDarkTheme
-                                    updateTheme(this@MainActivity, isDarkTheme)
-                                }
-                            )
-                        }
+                        {isDarkTheme = !isDarkTheme}
+                        updateTheme(context, !isDarkTheme)
                     }
                 }
             }
         }
     }
+}
+
+@PreviewScreenSizes
+@Composable
+fun MainActivityPreview() {
+    MainContent(MainActivity())
 }
